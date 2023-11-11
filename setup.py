2d68e004c668a35dca71e081e6b0b29c0cac3855
erev0s <erev0s@mail.com>
2023-11-11 10:14:13 +0200

    added convenience method to pass directly apk path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='apkInspector',
-    version='1.1.4',
+    version='1.1.5',
     author='erev0s',
     author_email='projects@erev0s.com',
     description='apkInspector is a tool designed to provide detailed insights into '
