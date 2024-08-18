8d5eec69818f8a820bd798138ffa9ff9ba9a7a0f
erev0s <12457993+erev0s@users.noreply.github.com>
2024-08-18 09:12:20 +0300

    Read from dir (#35)
    * cli to process also directory of apks
    
    * update version & funding

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,4 +1,3 @@
-import glob
 import io
 import os
 import unittest
@@ -7,7 +6,7 @@ import hashlib
 from apkInspector.extract import extract_file_based_on_header_info
 from apkInspector.headers import EndOfCentralDirectoryRecord, CentralDirectory, ZipEntry, LocalHeaderRecord
 from apkInspector.indicators import apk_tampering_check
-from apkInspector.axml import get_manifest, parse_apk_for_manifest
+from apkInspector.axml import get_manifest
 
 
 class ApkInspectorTestCase(unittest.TestCase):
