293ab2d89ab9ce011c7dbbc5df3c876172875a1c
erev0s <erev0s@mail.com>
2024-09-20 10:28:43 +0300

    update version for patch update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='apkInspector',
-    version='1.3.1',
+    version='1.3.2',
     author='erev0s',
     author_email='projects@erev0s.com',
     description='apkInspector is a tool designed to provide detailed insights into '
